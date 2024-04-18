24ea33e1197f5ec6950b44ce337101e9dc529ce4
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-04-17 23:39:35 -0400

    Did constructor tests

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -1,5 +1,6 @@
 package models;
 public class Sneaker {
+
     private int id;
     private String name;
     private String brand;
@@ -7,6 +8,28 @@ public class Sneaker {
     private int size;
     private int qty;
     private float price;
+    public Sneaker(String brand) {
+        this.brand = brand;
+    }
+    public Sneaker(int id) {
+        this.id = id;
+    }
+
+    public Sneaker(float price) {
+        this.price = price;
+    }
+
+    public Sneaker(int id, String name, String brand, String sport, int qty,float price) {
+        this.id=id;
+        this.name=name;
+        this.brand=brand;
+        this.sport=sport;
+        this.qty=qty;
+        this.price=price;
+    }
+    public Sneaker() {
+
+    }
     public void setName(String name) {
         this.name=name;
     }
