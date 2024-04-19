f4b7ff0fee159f47653619ac19ee91f6c076266e
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-04-19 09:56:36 -0400

    FINISHED

--- src/main/java/services/WhiskeyService.java
+++ src/main/java/services/WhiskeyService.java
@@ -1,3 +1,52 @@
+
 package services;
+import models.Sneaker;
+import models.Whiskey;
+
+import java.util.ArrayList;
+import java.util.List;
 public class WhiskeyService {
+    private static int nextId = 1;
+
+    private static List<Whiskey> inventory = new ArrayList<>();  // (2)
+
+
+    public static Whiskey create( String brand,float price) {
+       Whiskey createdWhiskey = new Whiskey(brand, nextId++,price);
+        inventory.add(createdWhiskey);
+        return createdWhiskey;
+    }
+
+
+    public Whiskey findWhiskey(int id) {
+        for  (Whiskey whiskey : inventory) {
+            if(whiskey.getId() == id) {
+                return whiskey;
+            }
+        }
+        return null;
+    }
+
+
+
+    public Whiskey[] findAll() {
+
+        Whiskey[] array = new Whiskey[inventory.size()];
+        return inventory.toArray(array);
+
+
+    }
+    public boolean delete(int id) {
+        for  (Whiskey whiskey : inventory) {
+            if(whiskey.getId() == id) {
+                inventory.remove(id);
+
+            }
+        }return true;
+
+    }
+
+
+
+
 }
--- src/test/java/services/WhiskeyServiceTest.java
+++ src/test/java/services/WhiskeyServiceTest.java
@@ -1,11 +1,34 @@
 package services;
+import models.Sneaker;
+import models.Whiskey;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class WhiskeyServiceTest {
 
     @Test
-    public void setIdTest(){
+        public void createTest(){
 
+            // (1)
+
+            String expectedBrand = "Adidas";
+            float expectedPrice = 80.00f;
+
+            // (2)
+            WhiskeyService whiskeyService = new WhiskeyService();
+            Whiskey testWhiskey = whiskeyService.create( expectedBrand,expectedPrice);
+
+            // (3)
+            int actualId = testWhiskey.getId();
+            String actualBrand = testWhiskey.getBrand();
+            float actualPrice = testWhiskey.getPrice();
+
+            // (4)
+            Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+            Assertions.assertEquals(expectedBrand, actualBrand);
+            Assertions.assertEquals(expectedPrice, actualPrice);
+
+        }
     }
 
 
-}
+
